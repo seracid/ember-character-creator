e6b626a4cacecfb79452f1390dff06fb35311fd5
seracid <seracid@gmail.com>
2017-09-03 23:20:13 -0500

    Adding initial files

---
+++ Mass-IV.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script makes an executable possible to just generate 
+# characters by double-clicking an icon on the desktop.
+
+#cd "/home/alan/Desktop/Ember Work/Generators/Character Generator/Data"
+#gnome-terminal --execute bash -c "./Mass-IV ; bash"
+
+cd ./Data
+gnome-terminal --execute bash -c ./Mass-IV
+
